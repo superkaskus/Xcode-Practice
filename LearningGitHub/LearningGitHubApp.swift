17ec7d4cd487aa146f02e7d542da71825d8b7487
Rahim <twarikadams@gmail.com>
2021-11-03 10:24:25 +1100

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Adam Abdul-rahim on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
